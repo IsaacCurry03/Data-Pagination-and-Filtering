410e2a0f790b5cdab2626b082ca6e21bde0381e8
IsaacCurry03 <73970390+IsaacCurry03@users.noreply.github.com>
2020-12-22 09:34:58 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,89 +1,78 @@
-const studentList = document.getElementsByClassName("student-item");
-const studentLimit = 9;
-let resultsFound = true;
+/*
+This show page function loops through the   array and display's each
+students information as a list item.
+*/
 
-
-
-
-
-
-
-//function get set number of pages
- const numberOfPages = (array, pageMax) => {
-    const totalPages = Math.ceil(array.length / studentLimit);
-    return totalPages;
-};
-
-//Show Page function
-const showPage = (array, pageNum) => {
-    //set index numbers to adjust for items in array with 0 based index
-    const startIndex = (pageNum * studentLimit) - studentLimit;
-    const endIndex = (pageNum * studentLimit) - 1;
-
-    const li = document.querySelector('li');
-    for (let i = 0; i < array.length; i++) {
-        array[i].style.display = 'none';
+function showPage(list, page) {
+  let startIndex = (page * 9) - 9;
+  let endIndex = page * 9;
+const ul = document.querySelector('.student-list');
+ul.innerHTML = "";
+for( let i = 0; i < list.length ; i++ ) {
+    if ( i >= startIndex && i < endIndex) {
+    ul.insertAdjacentHTML('beforeend', `
+        <li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+            <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+        </li>
+    `);
     }
-    for (let i = 0; i < array.length; i++) {
-        if (i >= startIndex && i <= endIndex) {
-            array[i].style.display = 'block';
-        }
-    }
-};
-
-/***
- Create the `appendPageLinks function` to generate, append, and add
- functionality to the pagination buttons.
- ***/
-const appendPageLinks = (list) => {
-    //get page from HTML
-    const page = document.querySelector('.page');
-    const div = document.createElement("div");
-    const pageList = document.createElement("ul");
-    div.classList.add('pagination');
-    page.appendChild(div);
-    div.appendChild(pageList);
-    const pagesNeeded = numberOfPages(studentList, studentLimit);
-    for (let i = 1; i <= pagesNeeded; i++) {
-        const li = document.createElement("li");
-        const a = document.createElement("a");
-        a.textContent = i.toString();
-        //set page 1 to active
-        if (a.textContent === '1') {
-            a.classList.add('active');
-        }
-        a.setAttribute('href', '#');
-        pageList.appendChild(li);
-        li.appendChild(a);
-    }
-    setEventHandlers();
-};
-const removePagination = () => {
-    const pagination = document.querySelector('.pagination');
-    console.log(pagination);
-    pagination.innerHTML = '';
 }
-
-const setEventHandlers = () => {
-    const anchorTags = document.querySelectorAll("a");
-    anchorTags.forEach(element => {
-        addEventListener("click", (e) => {
-            updateActiveLink(anchorTags);
-            e.target.classList.add('active');
-            showPage(studentList, e.target.textContent);
-      })
-   })
-}
-
-const updateActiveLink = (tagList) => {
-   tagList.forEach(element => {
-      if (element.classList.contains('active')) {
-            element.classList.remove('active');
-      }
-   })
 }
 
 
+/*
+this function counts how many page buttons 
+it should add from defined array
+*/
 
-showPage(studentList, 1);
-appendPageLinks(studentList);
+function addPagination(list) {
+const pageCount = Math.ceil(list.length / 9);
+const ul = document.querySelector('.link-list');
+ul.innerHTML = "";
+
+for ( let i = 0 ; i < pageCount ; i++ ) {
+    ul.insertAdjacentHTML('beforeend', `
+    <li>
+        <button type="button">${i + 1}</button>
+    </li>
+    `);
+}
+
+const buttons = document.querySelectorAll('.link-list button');
+buttons[0].classList.add('active');
+
+
+/* 
+    This Event Listener listen's for a click event.
+    
+  */
+
+ul.addEventListener('click', (event) => {
+    const button = event.target;
+    const page = button.textContent;
+
+    if (button.nodeName === "BUTTON" ) {
+
+    buttons.forEach(function(button){
+        button.classList.remove('active');
+    });
+
+    button.classList.add("active");
+    showPage(list, page);
+    }
+
+});
+
+};
+
+
+
+showPage(data, 1);
+addPagination(data);
